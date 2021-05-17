4b74b7c5ee42b9e1e3c48a17979b82373ea989af
Kalle Kromann <kalle.kromann@man-es.com>
2021-05-17 14:50:37 +0200

    [TOOLS-4787] Added command to update settings.

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,12 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as settingManager from './setting-manager';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	let commandId = 'man-vscode.get-settings';
-	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
+	context.subscriptions.push(vscode.commands.registerCommand('man-vscode.get-settings', () => {
 		settingManager.getSettings();
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('man-vscode.update-settings', () => {
+		settingManager.updateSettings();
 	}));
 }
--- src/setting-manager.ts
+++ src/setting-manager.ts
@@ -2,21 +2,20 @@ import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as fs from 'fs';
 
-export function getSettings() {
-    let allWorkspaceFolders = vscode.workspace.workspaceFolders;
-    if (allWorkspaceFolders === undefined) {
+export async function getSettings() {
+    let workspaceFolder = getWorkspaceFolder();
+    if (workspaceFolder === "") {
         vscode.window.showErrorMessage("Please open a workspace and try again.");
         return;
     }
-    let workspaceFolder = allWorkspaceFolders[0].uri.path;
 
-    workspaceFolder = sanitizePath(workspaceFolder);
-
+    // Verify that .vscode folder does not exist
     if (fs.existsSync(`${workspaceFolder}/.vscode`)) {
         vscode.window.showErrorMessage("Workspace already contains .vscode folder.");
         return;
     }
 
+    // Clone the vscode repository to .vscode folder
     vscode.window.withProgress({
         location: vscode.ProgressLocation.Notification,
         title: "Getting settings.",
@@ -30,25 +29,110 @@ export function getSettings() {
     });
 }
 
+export async function updateSettings() {
+    let workspaceFolder = getWorkspaceFolder();
+    if (workspaceFolder === "") {
+        vscode.window.showErrorMessage("Please open a workspace and try again.");
+        return;
+    }
+
+    let vscodeFolder = `${workspaceFolder}/.vscode`;
+    // Verify that .vscode folder exists
+    if (!fs.existsSync(vscodeFolder)) {
+        vscode.window.showErrorMessage("Workspace does not contain .vscode folder. Run command 'MAN: Get Settings' to download folder.");
+        return;
+    }
+
+    let vscodeGitFolder = `${vscodeFolder}/.git`;
+    // Verify that .vscode folder is our vscode git repository
+    if (!fs.existsSync(vscodeGitFolder)) {
+        vscode.window.showErrorMessage(".vscode folder is not a git repository. Delete/backup the folder and run command 'MAN: Get Settings' to download folder.");
+        return;
+    }
+
+    // Verify that the vscode repository has the expected remote URL
+    let remoteUrl = await execWithStdout(vscodeGitFolder, 'git', ['config', '--get', 'remote.origin.url'])
+    .then(function(result) {
+        return result.trim();
+    }, function(error) {
+        throw Error(error);
+    });
+    let urlRegex = /^https:\/\/.*git-unisource\.md-man\.biz:7990\/scm\/eeeabb\/vscode\.git$/;
+    if (!remoteUrl.match(urlRegex)) {
+        vscode.window.showErrorMessage(".vscode git repository does not have expected remote URL. Delete/backup the folder and run command 'MAN: Get Settings' to download folder.");
+        return;
+    }
+
+    // Pull any recent changes
+    execWithStdout(vscodeFolder, 'git', ['pull'])
+    .then(function(result) {
+        if (result.trim() === "Already up to date.") {
+            vscode.window.showInformationMessage(result);
+        } else {
+            vscode.window.showInformationMessage("Settings succesfully updated.");
+        }
+    }, function(error) {
+        vscode.window.showErrorMessage(error);
+    });
+}
+
+function getWorkspaceFolder() {
+    let allWorkspaceFolders = vscode.workspace.workspaceFolders;
+    if (allWorkspaceFolders === undefined) {
+        return "";
+    }
+    let workspaceFolder = allWorkspaceFolders[0].uri.path;
+
+    return sanitizePath(workspaceFolder);
+}
+
+function execWithStdout(workingDir: string, command: string, args: string[]) {
+    return new Promise<string>((resolve, reject) => {
+        const child = execCB(workingDir, command, args, function(output, code) {
+            if (code === 0) {
+                resolve(output);
+            } else {
+                reject(`'${command} ${args.join(' ')}' returned non-zero exit code ${code}`);
+            }
+        });
+    });
+}
+
+// Execute without callback function
 function exec(workingDir: string, command: string, args: string[]) {
+    _exec(workingDir, command, args, function(output, code) {});
+}
+
+// Execute with callback function
+function execCB(workingDir: string, command: string, args: string[], callback: (buffer: string, code: number) => void) {
+    _exec(workingDir, command, args, callback);
+}
+
+function _exec(workingDir: string, command: string, args: string[], callback: (buffer: string, code: number) => void) {
     const child = cp.spawn(command, args, {
         "cwd": workingDir
     });
 
+    let buffer = "";
+
     child.stdout.on('data', (data) => {
-        console.debug(`${data}`);
+        console.debug(`stdout: ${data}`);
+        buffer += data;
     });
 
     child.stderr.on('data', (data) => {
+        console.debug(`stderr: ${data}`);
         vscode.window.showErrorMessage(`ERROR: ${data}`);
     });
 
     child.on('error', (err) => {
+        console.debug(`error: ${err}`);
         vscode.window.showErrorMessage(`ERROR: ${err}`);
     });
 
     child.on('close', (code) => {
         console.debug(`child process exited with code ${code}`);
+        callback(buffer, code);
     });
 }
 
